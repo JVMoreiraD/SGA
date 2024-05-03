ebb31106d5963a5d16dc3cc02ed06ccb74c58015
jvmoreirad <jvitormoreirad@hotmail.com>
2024-05-03 00:17:36 -0300

    feat novas rotas

--- backend/main.go
+++ backend/main.go
@@ -14,16 +14,20 @@ func init() {
 }
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"Message": "pong",
-		})
-	})
+
 	r.POST("/signup", controllers.SignUp)
 	r.POST("/login", controllers.Login)
 	r.GET("/users", middleware.RequireAuth, controllers.GetUsers)
-
 	r.GET("/validate", middleware.RequireAuth, controllers.Validate)
 	r.POST("/keys", middleware.RequireAuth, controllers.KeysCreate)
+	r.GET("/keys", middleware.RequireAuth, controllers.GetKeys)
+	// r.PATCH("/keysToUser", middleware.RequireAuth, controllers.GiveKeyToUser)
+	r.POST("/lockerGroup", middleware.RequireAuth, controllers.CreateLockerGroup)
+	r.GET("/lockerGroup", middleware.RequireAuth, controllers.GetLockerGroup)
+	r.POST("/locker", middleware.RequireAuth, controllers.CreateLocker)
+	r.GET("/locker", middleware.RequireAuth, controllers.GetLockers)
+	r.POST("/roles", middleware.RequireAuth, controllers.CreateRole)
+	r.GET("/roles", middleware.RequireAuth, controllers.GetRoles)
+
 	r.Run()
 }
