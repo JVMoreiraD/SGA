3096bf9d55f201199969818f02449b7ab78d6cee
jvmoreirad <jvitormoreirad@hotmail.com>
2025-04-14 15:23:55 -0300

    feat: home

---
+++ frontend/sga/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from "@/components/login-form";
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
