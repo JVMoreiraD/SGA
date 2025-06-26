e47d6b5c72a6ec56a97e886d852bfd0ef375dbbe
JVMoreiraD <jvitormoreirad@hotmail.com>
2025-06-26 00:00:32 -0300

    wip: groups

---
+++ frontend/sga/src/app/dashboard/management/columns.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { ColumnDef } from "@tanstack/react-table"
+import { EditIcon, MoreHorizontal, Trash2Icon } from "lucide-react"
+
+export type Group = {
+    Name: string
+    Description: string
+}
+
+
+export const columns: ColumnDef<Group>[] = [
+    {
+        accessorKey: "Name",
+        header: "Name"
+    },
+    {
+        accessorKey: "Description",
+        header: "Descrição",
+    },
+    {
+        id: "actions",
+        enableHiding: false,
+        cell: ({}) => {
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Ações</DropdownMenuLabel>
+                        <DropdownMenuItem
+                        >
+                            <EditIcon />
+                            Editar
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>
+                            <Trash2Icon />
+                            Deletar
+                        </DropdownMenuItem>
+
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        },
+    },
+]
--- frontend/sga/src/app/dashboard/management/page.tsx
+++ frontend/sga/src/app/dashboard/management/page.tsx
@@ -1,23 +1,31 @@
-// 'use client'
-// import { Input } from "@/components/ui/input"
+import { Group, columns } from "./columns"
+import { DataTable } from "./dataTable"
 
+async function getData(): Promise<Group[]> {
+    // Fetch data from your API here.
+    return [
+        {
+            Name: "Professores",
+            Description: "Grupo de usuários dos professores"
+        },
+        {
+            Name: "Alunos",
+            Description: "Grupo de usuários dos alunos"
+        }, {
+            Name: "Terceirizados",
+            Description: "Grupo de usuários dos terceirizados"
+        }
+    ]
+}
 
 export default async function DemoPage() {
+    const data = await getData()
+
     return (
-        <div className="w-full">
+        <div className="w-full flex items-center justify-center">
+            <DataTable columns={columns} data={data} />
 
-            <div className="flex justify-center items-start py-4 gap-x-14">
-
-            </div>
-
-            <div className="flex items-center justify-center">
-
-
-
-
-            </div>
         </div>
-
     )
 }
 
